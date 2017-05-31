fd1bb383c33e3decf68bf70b9a3210c69eaf561d
mandeep <mandeep@m13a.me>
2017-05-31 17:49:58 +0100

    initial commit

---
+++ src/main/java/io/nanocode/hugs/Main.java
@@ -0,0 +1,95 @@
+package io.nanocode.hugs;
+
+import io.nanocode.hugs.commands.HugCommand;
+import net.minecraft.server.v1_11_R1.EnumParticle;
+import net.minecraft.server.v1_11_R1.PacketPlayOutWorldParticles;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.v1_11_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+import org.bukkit.entity.Snowball;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main extends JavaPlugin implements Listener {
+
+    public List<String> cooldowns = new ArrayList<>();
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+
+        getServer().getPluginManager().registerEvents(this, this);
+        registerCommands();
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    public void registerCommands() {
+        getCommand("huggun").setExecutor(new HugCommand(this));
+    }
+
+    @EventHandler
+    public void onEntityInteractEvent(PlayerInteractEvent event) {
+        Player p = event.getPlayer();
+        if (p.getInventory().getItemInMainHand().getType() == Material.getMaterial(getConfig().getInt("item-id")) && (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)) {
+            int cooldown = getConfig().getInt("cooldown");
+
+            if (cooldowns.contains(p.getUniqueId().toString())) {
+                p.sendMessage(ChatColor.RED + "[" + ChatColor.BOLD + "!" + ChatColor.RESET + ChatColor.RED.toString() + "] " + "You must wait " + cooldown + " seconds before firing the hug gun!");
+            } else {
+                p.launchProjectile(Snowball.class);
+                p.launchProjectile(Snowball.class);
+
+                if (cooldown != 0) {
+                    cooldowns.add(p.getUniqueId().toString());
+
+                    getServer().getScheduler().runTaskLater(this, new Runnable() {
+                        @Override
+                        public void run() {
+                            cooldowns.remove(p.getUniqueId().toString());
+                        }
+                    }, cooldown * 20);
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onProjectileHitEvent(ProjectileHitEvent event) {
+        Projectile p = event.getEntity();
+
+        if (p instanceof Snowball) {
+            PacketPlayOutWorldParticles packet = new PacketPlayOutWorldParticles(EnumParticle.HEART, true, p.getLocation().getBlockX(), p.getLocation().getBlockY(), p.getLocation().getBlockZ(), 1, 1, 1, 0, 5);
+
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                if (p.getLocation().distanceSquared(player.getLocation()) <= 30 * 30) {
+                    ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onEntityDamageByEntityEvent(EntityDamageByEntityEvent event) {
+        if(event.getDamager() instanceof Snowball) {
+            event.setCancelled(true);
+        }
+    }
+}
---
+++ src/main/java/io/nanocode/hugs/commands/HugCommand.java
@@ -0,0 +1,27 @@
+package io.nanocode.hugs.commands;
+
+import io.nanocode.hugs.Main;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class HugCommand implements CommandExecutor {
+    private Main main;
+
+    public HugCommand(Main main) {
+        this.main = main;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if(commandSender instanceof Player) {
+            Player p = (Player) commandSender;
+            p.getInventory().addItem(new ItemStack(Material.getMaterial(main.getConfig().getInt("item-id")), 1));
+        }
+
+        return true;
+    }
+}
